dda841b20086bbd9214c6ca658809d50b3633fb4
loathengine <loathengine@gmail.com>
2019-07-29 22:55:43 +0200

    LE

--- fsg.py
+++ fsg.py
@@ -4,10 +4,10 @@ import random
 with open('data/primary_biome.txt') as file:
     list_primary_biome = file.read().splitlines()
 
-with open('data/primary_topology') as file:
+with open('data/primary_topology.txt') as file:
     list_primary_topology = file.read().splitlines()
 
-with open('data/industry_raw') as file:
+with open('data/industry_raw.txt') as file:
     list_industry_raw = file.read().splitlines()
 
 with open('data/industry_crafts.txt') as file:
@@ -15,8 +15,6 @@ with open('data/industry_crafts.txt') as file:
 
 with open('data/industry_services.txt') as file:
     list_industry_services = file.read().splitlines()
-
-
 
 random.seed( 12345 )
 
@@ -27,17 +25,11 @@ settlement_wealth = random.randint(1, 6)
 settlement_age = random.randint(1,6)
 settlement_structures = ((settlement_population << 1) // (settlement_density)) >> 1
 
-list_primary_biome = ["temperate deciduous forest", "temperate rain forest", "tropical rain forest",
-                 "tropical seasonal forest", "grassland", "savanna", "taiga", "cold desert", "hot desert", "tundra",
-                 "glacier"]
-list_primary_topology = ["coastal", "river", "flat lands", "foot hills", "plateau", "mountain"]
-list_industry_raw = ["mining", "farming", "ranching", "forestry", "fishing", "foraging"]
-list_industry_crafts = ["smith", "tanner", "weaver", "mason", "carpenter", "butcher / meat packer"]
-list_industry_services = ["barber", "maidservant"]
+def primary_biome(lpb = list_primary_biome):
+    return random.choice(lpb)
 
-
-
-
+def primary_topology(lpt = list_primary_topology):
+    return random.choice(lpt)
 
 def industry_raw(lir = list_industry_raw, sp = settlement_population, sw = settlement_wealth):
     return random.choice(lir)
@@ -47,9 +39,6 @@ def industry_crafts(lir = list_industry_raw, lic = list_industry_crafts, sp = se
 
 def industry_services(lis = list_industry_services,lir = list_industry_raw, lic = list_industry_crafts, sp = settlement_population, sw = settlement_wealth):
     return random.choice(lis)
-
-primary_biome = (random.choice(list_primary_biome))
-primary_topology = (random.choice(list_primary_topology))
 
 print("settlement_name - " + settlement_name)
 print("settlement_population - " + str(settlement_population))
@@ -61,6 +50,6 @@ print("industry_raw - " + industry_raw())
 print("industry_crafts - " + industry_crafts())
 print("industry_crafts - " + industry_services())
 
-print(settlement_name + " is a " + primary_topology + " settlement located in the greater " + primary_biome + " area."
+print(settlement_name + " is a " + primary_topology() + " settlement located in the greater " + primary_biome() + " area."
       "  The " + industry_raw() + " industry supports the local " + industry_crafts() + ".  With a population of "
       + str(settlement_population) + " there are about " + str(settlement_structures) + " structures in town.")
