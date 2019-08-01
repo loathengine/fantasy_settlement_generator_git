1ec2519899c7ebfc4d76f5ca7d2769672913338a
loathengine <loathengine@gmail.com>
2019-08-02 00:21:58 +0200

    LE

--- fsg.py
+++ fsg.py
@@ -31,18 +31,27 @@ def parse_xml_topography():
     return xml_list
 
 
-def parse_xml_industry():
+def parse_xml_raw():
+    tree = ET.parse('data/industry.xml')
+    root = tree.getroot()
+    xml_list = []
+    for raw in root.iter('RAW'):
+        xml_list.append(raw.get('name'))
+    return xml_list
+
+
+def parse_xml_craft():
     tree = ET.parse('data/industry.xml')
     root = tree.getroot()
     xml_list = []
 
-    for b in root.iter('BIOME'):
-        biome = b.get('name')
-        for topo in b.iter('TOPOGRAPHY'):
+    for r in root.iter('RAW'):
+        raw = r.get('name')
+        for craft in r.iter('CRAFT'):
             name_weight = []
-            name = topo.get('name')
-            weight = topo.get('weight')
-            name_weight.append(biome)
+            name = craft.get('name')
+            weight = craft.get('weight')
+            name_weight.append(raw)
             name_weight.append(name)
             name_weight.append(weight)
             xml_list.append(name_weight)
@@ -55,7 +64,7 @@ settlement_density = random.randint(1, 6)
 settlement_wealth = random.randint(1, 6)
 settlement_age = random.randint(1, 6)
 settlement_structures = ((settlement_population << 1) // settlement_density) >> 1
-settlement_shops = (settlement_population // 150)
+settlement_shops_num = (settlement_population // 150)
 primary_biome = random.choice(parse_xml_biome())
 
 
@@ -74,34 +83,27 @@ def get_primary_topology(pb=primary_biome, xml_topo=parse_xml_topography()):
 primary_topology = get_primary_topology()
 
 
-def get_industry_raw(pt=primary_topology, sp=settlement_population, sw=settlement_wealth):
-     return "ERROR40030"
+def get_industry_raw(ir=parse_xml_raw()):
+     return random.choice(ir)
 
 
 industry_raw = get_industry_raw()
 
 
-def get_industry_crafts(pt=primary_topology, ir=industry_raw, lic=None, sp=settlement_population, sw=settlement_wealth):
-    if pt == 'coastal':
-        weighted_random_industry_crafts = ['smith'] * 1 + ['tanner'] * 1 + ['weaver'] * 1 + ['mason'] * 1 + [
-            'carpenter'] * 999 + ['butcher/salt packer'] * 1
-        return random.choice(weighted_random_industry_crafts)
-    elif pt == 'river':
-        weighted_random_industry_crafts = ['smith'] * 1 + ['tanner'] * 1 + ['weaver'] * 1 + ['mason'] * 1 + [
-            'carpenter'] * 999 + ['butcher/salt packer'] * 1
-        return random.choice(weighted_random_industry_crafts)
-    else:
-        return "ERROR400040"
+def get_settlement_shops(ssn=settlement_shops_num):
+    length=len(xml_topo)
+    topo_list = []
+    for topo in xml_topo:
+        if topo[0] == pb:
+            i = int(topo[2])
+            while i > 0:
+                topo_list.append(topo[1])
+                i -= 1
+    return random.choice(topo_list)
 
 
-industry_crafts = get_industry_crafts()
+settlement_shops = get_settlement_shops()
 
-
-def get_industry_services(lir=industry_raw, lic=industry_crafts, sp=settlement_population, sw=settlement_wealth):
-    return "ERROR400050"
-
-
-industry_services = get_industry_services()
 
 print("settlement_name - " + settlement_name)
 print("settlement_population - " + str(settlement_population))
@@ -109,11 +111,9 @@ print("settlement_density - " + str(settlement_density))
 print("settlement_wealth - " + str(settlement_wealth))
 print("settlement_age - " + str(settlement_age))
 print("settlement_structures - " + str(settlement_structures))
-print("settlement_shops - " + str(settlement_shops))
 print("primary_biome - " + primary_biome)
 print("primary_topology - " + primary_topology)
 print("industry_raw - " + industry_raw)
-print("industry_crafts - " + industry_crafts)
-print("industry_services - " + industry_services)
+print("settlement_shops_num - " + str(settlement_shops_num))
 
-print("\n\n" + str(get_industry_raw()))
+print(parse_xml_raw())
