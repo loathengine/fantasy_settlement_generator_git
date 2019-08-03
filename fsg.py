0b8c47c69f505a8f13c5583056fd64521c6c8d56
loathengine <loathengine@gmail.com>
2019-08-04 01:58:32 +0200

    LE

--- fsg.py
+++ fsg.py
@@ -3,6 +3,7 @@ import random
 
 random.seed(random.randint(1, 100000))
 
+
 def parse_xml_element(xml_file, element, attribute):
     """Takes a file and an element name and returns a list of every instance of that element."""
     tree = ET.parse(xml_file)
@@ -15,34 +16,47 @@ def parse_xml_element(xml_file, element, attribute):
     return unique_xml_list
 
 
-def weighted_element_xml(xml_file, element1, element2):
+def weighted_element_xml(xml_file, element1, element2, primary):
     tree = ET.parse(xml_file)
     root = tree.getroot()
     weighted_list = []
-    i=0
-
+    i = 0
     for e1 in root.iter(element1):
-        e1_name = e1.get('name')
-        for e2 in e1.iter(element2):
-            name = e2.get('name')
-            weight = e2.get('weight')
-            i = int(weight)
-            while i > 0:
-                weighted_list.append(name)
-                i -= 1
+        #print('e1 - ' + str(e1.get('name')))
+        #print(primary)
+        if str(e1.get('name')) == str(primary):
+            #print("    if 1 TRUE")
+            for e2 in e1.iter(element2):
+                #print('      e2 - ' + str(e2.get('name')))
+                name = e2.get('name')
+                #print('      name - ' + name)
+                weight = e2.get('weight')
+                #print('      weight - ' + weight)
+                i = int(weight)
+                while i > 0:
+                    #print('          adding ' + name + ' to list')
+                    weighted_list.append(name)
+                    i -= 1
+        #else:
+            #print('if 1 FALSE')
+        #print (weighted_list)
     return random.choice(weighted_list)
 
 
 def get_settlement_shops(ssn, ir, xml_shop):
+    print('ir is - ' + ir)
     length = len(xml_shop)
     shop_list = []
     shop_results = ""
     shop_dict = {}
     i = 0
-    x = 0
+    x = length
     while x < ssn:
         x += 1
+        print('xml_shop is - ' + str(xml_shop))
         for shop in xml_shop:
+            print(shop)
+            print(ir)
             if shop[0] == ir:
                 i = int(shop[2])
             while i > 0:
@@ -56,7 +70,6 @@ def get_settlement_shops(ssn, ir, xml_shop):
     return shop_dict
 
 
-
 settlement_name = "Testberg"
 settlement_population = random.randint(100, 5000)
 settlement_density = random.randint(1, 6)
@@ -64,28 +77,30 @@ settlement_wealth = random.randint(1, 6)
 settlement_age = random.randint(1, 6)
 settlement_structures = ((settlement_population << 1) // settlement_density) >> 1
 settlement_shops_num = (settlement_population // 150)
-settlement_shops_list = (parse_xml_element('data/biome.xml', 'ENV', 'SHOP'))
-primary_biome = weighted_element_xml('data/biome.xml', 'ENV', 'BIOME')
-primary_topology = weighted_element_xml('data/biome.xml', 'BIOME', 'TOPOGRAPHY')
-industry_raw = weighted_element_xml('data/biome.xml', 'TOPOGRAPHY', 'RAW')
-#settlement_shops = get_settlement_shops(settlement_shops_num, industry_raw, settlement_shops_list)
+settlement_shops_list = (parse_xml_element('data/biome.xml', 'SHOP', 'name'))
+# primary_biome = weighted_element_xml('data/biome.xml', 'ENV', 'BIOME')
+# primary_topology = weighted_element_xml('data/biome.xml', 'BIOME', 'TOPOGRAPHY')
+# industry_raw = weighted_element_xml('data/biome.xml', 'TOPOGRAPHY', 'RAW')
+# settlement_shops = get_settlement_shops(settlement_shops_num, industry_raw, settlement_shops_list)
 
-print("settlement_name - " + settlement_name)
-print("settlement_population - " + str(settlement_population))
-print("settlement_density - " + str(settlement_density))
-print("settlement_wealth - " + str(settlement_wealth))
-print("settlement_age - " + str(settlement_age))
-print("settlement_structures - " + str(settlement_structures))
-print("primary_biome - " + primary_biome)
-print("primary_topology - " + primary_topology)
-print("industry_raw - " + industry_raw)
-print("settlement_shops_num - " + str(settlement_shops_num))
+# print("settlement_name - " + settlement_name)
+# print("settlement_population - " + str(settlement_population))
+# print("settlement_density - " + str(settlement_density))
+# print("settlement_wealth - " + str(settlement_wealth))
+# print("settlement_age - " + str(settlement_age))
+# print("settlement_structures - " + str(settlement_structures))
+# print("primary_biome - " + primary_biome)
+# print("primary_topology - " + primary_topology)
+# print("industry_raw - " + industry_raw)
+# print("settlement_shops_num - " + str(settlement_shops_num))
+# print("settlement_shop_list - " + str(settlement_shops_list))
+# print('     Shop Type -- Number')
+# print('     -------------------')
 
-print('     Shop Type -- Number')
-print('     -------------------')
-#for x,y in settlement_shops.items():
+# for x,y in settlement_shops.items():
 #    print('     ' + x + " " + str(y))
 
-
-get_settlement_shops(settlement_shops_num, 'mining', settlement_shops_list)
-
+i = 0
+while i < 10:
+    i += 1
+    print(weighted_element_xml('data/biome.xml', 'RAW', 'SHOP', 'mining'))
