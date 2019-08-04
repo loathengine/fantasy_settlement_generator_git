99367d7236a754a0ba8e38b606e9ae3534dce6ae
loathengine <loathengine@gmail.com>
2019-08-04 16:02:56 +0200

    LE

--- make_xml.py
+++ make_xml.py
@@ -1,7 +1,7 @@
 with open('./data/primary_biome.txt') as b:
     biome = b.read().splitlines()
 
-with open('./data/primary_topology.txt') as t:
+with open('./data/primary_topography.txt') as t:
     topo = t.read().splitlines()
 
 with open('./data/industry_raw.txt') as r:
@@ -16,9 +16,9 @@ print('<ENV>')
 for b in biome:
     print('    <BIOME name="' + b + '" weight="3" >')
     for t in topo:
-        print('        <TOPOLOGY name="' + t + '" weight="3" >')
+        print('        <TOPOGRAPHY name="' + t + '" weight="3" >')
         for r in raw:
             print('            <RAW name="' + r + '" weight="3" />')
-        print('        </TOPOLOGY>')
+        print('        </TOPOGRAPHY>')
     print('    </BIOME>')
 print('</ENV>')
