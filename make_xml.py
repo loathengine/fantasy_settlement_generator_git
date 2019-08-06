0926f62d8ffe7d73a817ae43b836f18adc8ddb0f
loathengine <loathengine@gmail.com>
2019-08-07 00:03:15 +0200

    LE

--- make_xml.py
+++ make_xml.py
@@ -1,42 +1,45 @@
-with open('./data/primary_biome.txt') as b:
-    biome = b.read().splitlines()
-
-with open('./data/primary_topography.txt') as t:
-    topo = t.read().splitlines()
-
-with open('./data/industry_raw.txt') as r:
-    raw = r.read().splitlines()
-
-with open('./data/industry_crafts.txt') as cs:
-    craft_shop = cs.read().splitlines()
-
-with open('./data/industry_services.txt') as ss:
-    service_shop = ss.read().splitlines()
 
 
-print('<?xml version="1.0" encoding="UTF-8"?>')
-print('<UNIVERSE>')
-print('  <ENV>')
-for b in biome:
-    print('    <BIOME name="' + b + '" weight="3" >')
+def write_xml(file):
+    with open('./data/primary_biome.txt') as b:
+        biome = b.read().splitlines()
+
+    with open('./data/primary_topography.txt') as t:
+        topo = t.read().splitlines()
+
+    with open('./data/industry_raw.txt') as r:
+        raw = r.read().splitlines()
+
+    with open('./data/industry_crafts.txt') as cs:
+        craft_shop = cs.read().splitlines()
+
+    with open('./data/industry_services.txt') as ss:
+        service_shop = ss.read().splitlines()
+
+    print('<?xml version="1.0" encoding="UTF-8"?>', file=file)
+    print('<UNIVERSE>', file=file)
+    print('  <ENV>', file=file)
+    for b in biome:
+        print('    <BIOME name="' + b + '" weight="3" >', file=file)
+        for t in topo:
+            print('      <TOPOGRAPHY name="' + t + '" weight="3" />', file=file)
+        print('    </BIOME>', file=file)
+    print('  </ENV>', file=file)
+
+    print('  <IND>', file=file)
     for t in topo:
-        print('      <TOPOGRAPHY name="' + t + '" weight="3" />')
-    print('    </BIOME>')
-print('  </ENV>')
-
-print('  <IND>')
-for t in topo:
-    print('    <TOPOGRAPHY name="' + t + '" weight="3" >')
-    for r in raw:
-        print('      <RAW name="' + r + '" weight="3" >')
-        for cs in craft_shop:
-            print('        <SHOP name="' + cs + '" weight="3" />')
-        for ss in service_shop:
-            print('        <SHOP name="' + ss + '" weight="3" />')
-        print('      </RAW>')
-    print('    </TOPOGRAPHY>')
-print('  </IND>')
-print('</UNIVERSE>')
+        print('    <TOPOGRAPHY name="' + t + '" weight="3" >', file=file)
+        for r in raw:
+            print('      <RAW name="' + r + '" weight="3" >', file=file)
+            for cs in craft_shop:
+                print('        <SHOP name="' + cs + '" weight="3" />', file=file)
+            for ss in service_shop:
+                print('        <SHOP name="' + ss + '" weight="3" />', file=file)
+            print('      </RAW>', file=file)
+        print('    </TOPOGRAPHY>', file=file)
+    print('  </IND>', file=file)
+    print('</UNIVERSE>', file=file)
 
 
-
+with open("data/monolith.xml", "w") as m:
+    write_xml(m)
