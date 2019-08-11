ff1c013ab42e2a67239998fd577207c1babd414d
loathengine <loathengine@gmail.com>
2019-08-11 02:35:52 +0200

    LE

--- data/make_xml2.py
+++ data/make_xml2.py
@@ -30,21 +30,21 @@ def write_xml
     for s_l in size_label:
         print('    <LABEL name="' + str(s_l['key']) + '" ceiling="' + str(s_l['weight']) + '" />', file=file)
     for s_g in settlement_government:
-        print('    <GOVERNMENT name="' + s_g + '" weight="3" />', file=file)
+        print('    <GOVERNMENT name="' + str(s_g['key']) + '" weight="3" />', file=file)
     for s_t in settlement_trait:
-        print('    <TRAIT name="' + s_t + '" weight="3" />', file=file)
+        print('    <TRAIT name="' + str(s_t['key']) + '" weight="3" />', file=file)
     print('  </STATS>', file=file)
     print('  <ENV>', file=file)
     for p_b in primary_biome:
-        print('    <BIOME name="' + p_b + '" weight="3" >', file=file)
+        print('    <BIOME name="' + str(p_b['key']) + '" weight="3" >', file=file)
         for p_t in primary_topography:
-            print('    <TOPOGRAPHY name="' + p_t + '" weight="3" >', file=file)
+            print('    <TOPOGRAPHY name="' + str(p_t['key']) + '" weight="3" >', file=file)
             for r_i in industry_raw:
-                print('      <RAW name="' + r_i + '" weight="3" >', file=file)
+                print('      <RAW name="' + str(r_i['key']) + '" weight="3" >', file=file)
                 for i_c in industry_crafts:
-                    print('        <SHOP name="' + i_c + '" weight="3" />', file=file)
+                    print('        <SHOP name="' + str(i_c['key']) + '" weight="3" />', file=file)
                 for i_s in industry_services:
-                    print('        <SHOP name="' + i_s + '" weight="3" />', file=file)
+                    print('        <SHOP name="' + str(i_s['key']) + '" weight="3" />', file=file)
                 print('      </RAW>', file=file)
             print('    </TOPOGRAPHY>', file=file)
         print('    </BIOME>', file=file)
