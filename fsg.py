4739812ceac6f51b8458fd8496ceb51e9285694d
loathengine <loathengine@gmail.com>
2019-08-18 21:11:34 +0200

    LE

--- fsg.py
+++ fsg.py
@@ -1,4 +1,5 @@
 #TODO:add functionality
+# figure out race stuff
 # shop number and quality
 # government and guards
 # religion and stuff
@@ -68,7 +69,7 @@ def get_settlement_label(xml_file, element_root, settlement_population):
 
 
 settlement_name = "Testberg"
-settlement_population = random.randint(1, 50)
+settlement_population = random.randint(20, 50000)
 settlement_label = get_settlement_label('data/monolith.xml', "./STATS/LABEL", settlement_population)
 settlement_density = random.randint(3, 6)
 settlement_wealth = random.randint(1, 6)
@@ -86,11 +87,14 @@ industry_raw = weighted_element_xml('data/monolith.xml', "./ENV/BIOME/TOPOGRAPHY
 settlement_shops = get_settlement_shops(settlement_shops_num)
 
 print("# " + settlement_name)
+print("\n")
 print("<div class='wide'>")
 print("<img src='https://i.imgur.com/iMA7lg9.png' style='width:700px' />")
 print("</div>")
+print("\n")
 print("### Background")
 print("Add background flavor.")
+print("\n")
 print("## Settlement Features")
 print(settlement_name + " is a " + settlement_label + " located in the " + primary_topography + " region of the areas "
       + "greater " + primary_biome + ".  The settlement seems to be " + settlement_age + ".  " + settlement_name +
@@ -106,32 +110,41 @@ print("- **Age: **" + str(settlement_age))
 print("- **Alignment: **" + str(settlement_alignment))
 print("- **Government Type: **" + settlement_government)
 print("- **Settlement Trait: **" + settlement_trait)
-
+print("\n")
 print("#### Industry and Economy")
 print("___")
 print("- **Primary Raw Materials: **" + industry_raw)
 print("- **Number Of Structures: **" + str(settlement_structures))
+print("- **Number Of Shops: **" + str(settlement_shops_num))
 print("- **Shops of Note: **")
 for x,y in settlement_shops.items():
-    print(x)
+    print(x + ",")
+print("\n")
+print("#### Food and Hospitality")
+print("___")
+print("- **Available Inns: **" + str(settlement_inn_num))
+print("- **Operating Taverns: **" + str(settlement_tavern_num))
+print("\n")
+print("### Districts")
+print("1. Receb's Yards - This area contains the spiritual grounds of Areksul.  Named after the bishop Receb whom for decades tirelessly spent his fortune turning the area into a beautiful place for all to worship.  A true Omnist.  'Receb the Purifier' was found dead having choked to death ravenously feasting on the entrails of children.  The remains of thousands where found in his personal home.  No one quite knows where the children came from because the number would be unsupportable by even a large city.  The lanes are lined with husks of worship much like a row of rotten teeth in the mandible of a long dead carnivore.  New and old are indistinguishable.  Sometimes you can here the trumpets of a seeker finding their faith.  No many local residents enter this area.")
+print("2. Wodogrenk Point - Contains a barracks and large parade grounds.  While well maintained the barracks is minimally staffed by ordinary road guard.")
+print("3. Clottiam West - Is quit old and houses not only the mayors house but also a few inns and taverns along with other businesses.  The building are quite ancient but well maintain and actually quite cozy.")
+print("4. Five Bowls - The west gate is where most of the trade occurs.  To the north is a shared square where a small but seeming perpetual farmers market exists.  This district seem to support not just the locals but also the tradesmen and travelers on road from the capital.")
+print("\n")
+print("### Notable Inns/Taverns")
+print("## Carpenter's Cup")
+print("#####  Location")
+print("In a Clottiam West ward, near an outcrop of rune-carved stone.")
+print("##### Description")
+print("The inn is a simple wooden shack, with several shuttered windows. Several battered shields hang on the walls. Accomodations consist of a few small rooms with straw mats and straw mats near the hearth.")
+print("##### Innkeeper")
+print("The innkeeper is a tall male human named Monder.")
+print("##### Menu")
+print("* Boiled Eggs and Dried Leek, Tankard of Stout (10 cp)")
+print("* Roasted Mutton and Dried Turnip, Tankard of Mead (10 cp)")
+print("* Stewed Onions, Mug of Cider (5 cp)")
+print("* Stewed Lentils, Mug of Cider (3 cp)")
+print("* Roasted Cabbage, Mug of Stout (4 cp)")
+print("* Stewed Lentils, Mug of Perry (4 cp)")
+print("* Pottage, Mug of Perry (4 cp)")
 
-
-print("settlement_population - " + str(settlement_population))
-print("settlement_label - " + settlement_label)
-print("settlement_wealth - " + str(settlement_wealth))
-print("settlement_age - " + str(settlement_age))
-print("settlement_alignment - " + str(settlement_alignment))
-print("settlement_government - " + settlement_government)
-print("settlement_trait - " + settlement_trait)
-print("settlement_structures - " + str(settlement_structures))
-print("primary_biome - " + primary_biome)
-print("primary_topography - " + primary_topography)
-print("industry_raw - " + industry_raw)
-print("settlement_inn_num - " + str(settlement_inn_num))
-print("settlement_tavern_num - " + str(settlement_tavern_num))
-print("settlement_shops_num - " + str(settlement_shops_num))
-
-#print('     Shops of Note')
-#print('     -------------------')
-#for x,y in settlement_shops.items():
-#    print('     ' + x)
